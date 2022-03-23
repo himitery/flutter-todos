92210e4cb06e1999a39967a99f3c2ed401ce3758
himitery <himit0131@gmail.com>
2022-03-24 00:46:37 +0900

    feat: 아이템을 로컬 저장소에 저장 - new dependence: shared_preferences

--- lib/todo/controller/todo_item_controller.dart
+++ lib/todo/controller/todo_item_controller.dart
@@ -1,13 +1,29 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../interfaces/todo_item.dart';
 
 class TodoItemController extends GetxController {
   RxList<TodoItem> todoItems = RxList();
 
+  @override
+  void onInit() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    todoItems.addAll(prefs
+            .getStringList('todoItems')
+            ?.map((element) => TodoItem.fromJson(jsonDecode(element))) ??
+        []);
+
+    super.onInit();
+  }
+
   addItem(TodoItem item) {
     todoItems.add(item);
     todoItems.sort((prev, next) => next.date.compareTo(prev.date));
+    _saveItemToLocalStorage();
   }
 
   updateItem(TodoItem item) {
@@ -16,11 +32,13 @@ class TodoItemController extends GetxController {
       todoItems.removeWhere((element) => element.key == item.key);
       todoItems.add(item);
       todoItems.sort((prev, next) => next.date.compareTo(prev.date));
+      _saveItemToLocalStorage();
     }
   }
 
   deleteItem(String key) {
     todoItems.removeWhere((element) => element.key == key);
+    _saveItemToLocalStorage();
   }
 
   checkItem(String key) {
@@ -28,5 +46,13 @@ class TodoItemController extends GetxController {
     TodoItem item = todoItems[index];
     item.isChecked = !item.isChecked;
     todoItems[index] = item;
+    _saveItemToLocalStorage();
+  }
+
+  _saveItemToLocalStorage() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.setStringList("todoItems",
+        todoItems.map((element) => jsonEncode(element.toJson())).toList());
   }
 }
--- lib/todo/interfaces/todo_item.dart
+++ lib/todo/interfaces/todo_item.dart
@@ -5,4 +5,14 @@ class TodoItem {
   bool isChecked;
 
   TodoItem(this.key, this.content, this.date, this.isChecked);
+
+  factory TodoItem.fromJson(Map<String, dynamic> json) => TodoItem(json["key"],
+      json["content"], DateTime.parse(json["date"]), json["isChecked"]);
+
+  Map<String, dynamic> toJson() => {
+        "key": key,
+        "content": content,
+        "date": date.toIso8601String(),
+        "isChecked": isChecked,
+      };
 }
